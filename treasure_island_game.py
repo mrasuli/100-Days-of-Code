4cad3d239858e394f391df9027300415cec64b84
mrasuli <khalisrasuli@live.co.uk>
2022-06-28 10:28:21 +0100

    Options changed

--- treasure_island_game.py
+++ treasure_island_game.py
@@ -5,16 +5,16 @@ print("Your mission is to find the treasure.")
 
 #the code 2nd attempt ....
 
-choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
-if choice1 == "left":
-  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
-  if choice2 == "wait":
-    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
-    if choice3 == "red":
+option1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if option1 == "left":
+  option2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+  if option2 == "wait":
+    option3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+    if option3 == "red":
       print("It's a room full of fire. Game Over.")
-    elif choice3 == "yellow":
+    elif option3 == "yellow":
       print("You found the treasure! You Win!")
-    elif choice3 == "blue":
+    elif option3 == "blue":
       print("You enter a room of beasts. Game Over.")
     else:
       print("You chose a door that doesn't exist. Game Over.")
